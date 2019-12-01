596a3d9c9c063a53cf7da40a729caea5bd4b0e97
2TComporation <58370054+2TComporation@users.noreply.github.com>
2019-12-01 11:29:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSmartLockInf(){
+var x = document.getElementById("SMARTLOCKINF");
+var y = document.getElementById("IMAGINEINF");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "block"
+  } else {
+    x.style.display = "none";
+  }
+}
